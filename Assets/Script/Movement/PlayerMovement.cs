4a793416062ea484debc7efef1568a1f419c9cc7
luanhuupuannn <luanhuupuan@gmail.com>
2024-09-30 23:23:17 +0700

    fixAnimation1(NguMaiDayLam)

--- Assets/Script/Movement/PlayerMovement.cs
+++ Assets/Script/Movement/PlayerMovement.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //player
-    private CharacterController controller;
+    public CharacterController controller;
+
     public float speed = 12f;
-    //jump
-    public float gravity = -9.81f * 2;
-    public float jumpHeight = 3f;
+    public float gravity = -9.81f;
+    public
+ float jumpHeight = 3f;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -17,114 +17,35 @@ public class PlayerMovement : MonoBehaviour
 
     Vector3 velocity;
     bool isGrounded;
-    bool isMoving;
 
-   public Animator animations;
-
-
-    private Vector3 lastPosition = new Vector3(0f, 0f, 0f);
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        controller = GetComponent<CharacterController>();
-       
-    }
-
-
-    private void Update()
-    {
-        if (Input.GetKey(KeyCode.W))
-        {
-            animations.SetBool("run", true);
-        }
-        else
-        {
-            animations.SetBool("run", false);
-        }
-    }
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        CheckGroundStatus();
-        HandleMovement();
-        HandleJump();
-        ApplyGravity();
-        CheckIfMoving();
-    }
-
-    // Ki?m tra n?u nh�n v?t ?ang tr�n m?t ??t
-    void CheckGroundStatus()
+    void
+ Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if (isGrounded && velocity.y < 0)
         {
-            velocity.y = -2f;
+            velocity.y
+ = -2f;
         }
-    }
 
-    // X? l� di chuy?n
-    void HandleMovement()
-    {
-        float x = Input.GetAxisRaw("Horizontal");
-        float z = Input.GetAxisRaw("Vertical");
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        Vector3
+ move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
-       
 
-    }
 
-    // X? l� nh?y
-    void HandleJump()
-    {
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
-            animations.SetBool("jump", true);
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-            Invoke("offjump", 2f);
-        }
-       
-    
-void offjump()
-{
-    animations.SetBool("jump", false);
-}
-
-// �p d?ng tr?ng l?c
-void ApplyGravity()
-    {
         velocity.y += gravity * Time.deltaTime;
+
         controller.Move(velocity * Time.deltaTime);
-    }
 
-    // Ki?m tra n?u nh�n v?t ?ang di chuy?n
-    void CheckIfMoving()
-    {
-        if (lastPosition != gameObject.transform.position && isGrounded == true)
-        {
-            isMoving = true;
-
-            animations.SetBool("run", true);
-          
-
-        }
-        else
-        {
-            isMoving = false;
-
-            animations.SetBool("run", false);
-           
-
-
-        }
-
-        lastPosition = gameObject.transform.position;
     }
 }
