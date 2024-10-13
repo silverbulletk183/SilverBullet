f18b12cfe6b339d06380400528e493fbbded75ff
Ginh1 <quangvinh3122@gmail.com>
2024-10-13 12:19:58 +0700

    UI Setting V.1.0.1

--- Assets/Script/Vinh/SoundSettings.cs
+++ Assets/Script/Vinh/SoundSettings.cs
@@ -1,47 +1,18 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
-using UnityEngine.UIElements;
 
 public class SoundSettings : MonoBehaviour
 {
-    private Slider musicSlider;
-    private Slider sfxSlider;
-    public AudioMixer audioMixer;
-
-    public static event Action<float> OnMusicVolumeChanged;
-    public static event Action<float> OnSFXVolumeChanged;
-
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        var root = GetComponent<UIDocument>().rootVisualElement;
-        musicSlider = root.Q<Slider>("MusicSlider");
-        sfxSlider = root.Q<Slider>("SFXSlider");
-        float musicVolume;
-        float sfxVolume;
-
-        if (audioMixer.GetFloat("Music", out musicVolume))
-        {
-            musicSlider.value = Mathf.Pow(10, musicVolume / 20);
-            Debug.Log(musicSlider.value);
-        }
-
-        if (audioMixer.GetFloat("SFX", out sfxVolume))
-        {
-            sfxSlider.value = Mathf.Pow(10, sfxVolume / 20);
-        }
-
-        musicSlider.RegisterValueChangedCallback(evt => MusicSliderChange(evt.newValue));
-        sfxSlider.RegisterValueChangedCallback(evt => SFXSliderChange(evt.newValue));
+        
     }
-    public void MusicSliderChange(float newValue)
-    {
-        Debug.Log($"Updated music volume in AudioManager: {newValue}");
-        OnMusicVolumeChanged?.Invoke(newValue);
-    }public void SFXSliderChange(float newValue)
-    {
-        Debug.Log($"Updated SFX volume in AudioManager: {newValue}");
 
-        OnSFXVolumeChanged?.Invoke(newValue);
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
--- Assets/Script/Vinh/UI/UIStartScreen.cs
+++ Assets/Script/Vinh/UI/UIStartScreen.cs
@@ -13,11 +13,11 @@ public class UIStartScreen : MonoBehaviour
         optionsBtn = root.Q<Button>("Options_btn");
 
 
-        optionsBtn.clicked += GameEvent.OptionButtonOnClick.Invoke;
+        optionsBtn.RegisterCallback<ClickEvent>(evt => GameEvent.OptionButtonOnClick?.Invoke());
     }
     private void OnDisable()
     {
-        optionsBtn.clicked -= GameEvent.OptionButtonOnClick.Invoke;
+        optionsBtn.clicked -= GameEvent.OptionButtonOnClick;
     }
 
 }
