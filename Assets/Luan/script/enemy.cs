3f3d39f8fe805e891e6c9e1235530b29ecdeb0a1
luanhuupuannn <luanhuupuan@gmail.com>
2024-11-20 23:35:19 +0700

    thanhmau2

--- Assets/Luan/script/enemy.cs
+++ Assets/Luan/script/enemy.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class enemy : MonoBehaviour
 {
+    public ThanhMau thanhMau;
+    public int hphientai;
+    public int hptoida=100;
+
+    private void Start()
+    {
+        hphientai = hptoida;
+        thanhMau.capnhatthanhmau(hphientai, hptoida);
+    }
     public void OnHitByLaser()
     {
+        hphientai -= 1;
+        thanhMau.capnhatthanhmau(hphientai, hptoida);
         // Xử lý khi bị bắn bởi tia laser
         Debug.Log(gameObject.name + " bị bắn bởi tia laser!");
 
