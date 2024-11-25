a2e2675f8ea0e8cd52b6ca38e05fd1083e7cee9f
luanhuupuannn <luanhuupuan@gmail.com>
2024-11-25 14:21:57 +0700

    up bansung

--- Assets/Fps Full Body Tutorial/HamzaKhan/Scripts/Decals/FleshDecal.cs
+++ Assets/Fps Full Body Tutorial/HamzaKhan/Scripts/Decals/FleshDecal.cs
@@ -1,13 +1,33 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class FleshDecal : MonoBehaviour, IHitable
 {
 
     [SerializeField] private GameObject decalPrefab;
 
+
+
+    public ThanhMau thanhMau;
+    public int hphientai;
+    public int hptoida = 100;
+
+    private void Start()
+    {
+        hphientai = hptoida;
+        thanhMau.capnhatthanhmau(hphientai, hptoida);
+    }
+
     public void Hit(GameObject hitObject, Vector3 hitPoint, Vector3 hitNormal)
     {
         GameObject decal = Instantiate(decalPrefab, hitPoint, Quaternion.LookRotation(hitNormal));
         decal.transform.SetParent(hitObject.transform);
+
+        hphientai -= 90;
+        thanhMau.capnhatthanhmau(hphientai, hptoida);
+        // Xử lý khi bị bắn bởi tia laser
+        Debug.Log(gameObject.name + " bị bắn bởi tia laser!");
+
+        // Thực hiện các hành động khác (giảm máu, hiệu ứng, v.v.)
+
     }
 }
